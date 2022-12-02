94130b9c2abc41505b4ff9346ea9d4475aec8f5b
travelite <rendrabootcamp@gmail.com>
2022-12-02 09:24:24 +0700

    fix card detail

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -84,7 +84,7 @@ const Detail = () => {
                 <img
                   src={`https://image.tmdb.org/t/p/original${detail.poster_path}`}
                   alt=""
-                  className="rounded-xl top-48 left-10 fixed w-60 shadow-lg  shadow-white/10"
+                  className="rounded-xl top-40 left-10 fixed w-60 shadow-lg  shadow-white/10"
                 />
                 <div className="  text-white flex flex-wrap  content-center m-auto items-center mb-16  ">
                   <div
